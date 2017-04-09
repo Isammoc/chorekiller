ac988f09bc36d2fdade44281672ceb8a926ac27b
Isammoc OFF <Isammoc@users.noreply.github.com>
2017-04-09 16:06:57 +0200

    feat(front): show connected user

--- front/src/app/login/login.component.ts
+++ front/src/app/login/login.component.ts
@@ -2,14 +2,25 @@ import { Component } from '@angular/core';
 import { MdDialog } from '@angular/material';
 import { LoginDialogComponent } from './login.dialog';
 
+import { UserService } from '../service/user/user.service';
+
+import { User } from '../service/user/user';
+
 @Component({
     selector: 'ck-login',
     templateUrl: './login.component.html',
 })
 export class LoginComponent {
-    constructor(private _dialog: MdDialog) {}
+  user: User;
 
-    openDialog() {
-        this._dialog.open(LoginDialogComponent);
-    }
+  constructor(private dialog: MdDialog, userService: UserService) {
+    userService.user.subscribe(user => {
+        console.log('Getting a new user to display');
+        this.user = user;
+      });
+  }
+
+  openDialog() {
+    this.dialog.open(LoginDialogComponent);
+  }
 }
--- front/src/app/service/user/user.service.ts
+++ front/src/app/service/user/user.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
 import { Observable } from 'rxjs/Observable';
+import { Subscriber } from 'rxjs/Subscriber';
 
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/share';
@@ -9,17 +10,35 @@ import { User } from './user';
 
 @Injectable()
 export class UserService {
-  baseUrl: '/api/users';
+  private baseUrl: '/api/users';
+  private userObs: Observable<User>;
+  private userSubscribers: Subscriber<User>;
 
-  constructor(private http: Http) {}
+  constructor(private http: Http) {
+    if (!this.userObs) {
+      this.userObs = new Observable<User>(observers => this.userSubscribers = observers)
+        .share();
+    }
+  }
+
+  get user() {
+    return this.userObs;
+  }
 
   login(login: string, passwd: string) {
     return new Observable<User>(observer => {
       if (login === 'admin' && passwd === 'changeit') {
-        observer.next({login: 'admin', displayName: 'Administrateurr'});
+        const user = {login: 'admin', displayName: 'Administrateurr'} as User;
+        this.setUser(user);
+        observer.next(user);
       } else {
         observer.error(400);
+        this.setUser(null);
       }
     });
+  }
+
+  private setUser(user: User) {
+    this.userSubscribers.next(user);
   }
 }
