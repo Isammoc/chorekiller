c6c4f2edf49f0f9b9b6fb1d1e32447fa4e8baafd
Isammoc OFF <Isammoc@users.noreply.github.com>
2017-07-16 17:08:37 +0200

    feat(ui): enhance TODO panel

--- front/src/app/under-construction/under-construction.component.ts
+++ front/src/app/under-construction/under-construction.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'ck-under-construction',
+  selector: 'ck-todo',
   templateUrl: './under-construction.component.html'
 })
 export class UnderConstructionComponent {
